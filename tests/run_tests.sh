90fbdc5379e2bf99d79e295968f52e6ae8e790a8
brian.ogrady@datastax.com <brian.ogrady@datastax.com>
2025-03-20 22:57:57 -0400

    Fixing utils test

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -4,5 +4,10 @@ uv pip install -e ".[all,dev]"
 source .venv/bin/activate
 python -m unittest tests/test_vector_column_options.py
 python -m unittest tests/test_astra_multi_vector_table.py
+python -m unittest tests/late_interaction/test_utils.py
+#python -m unittest tests/late_interaction/test_base.py
+#python -m unittest tests/late_interaction/test_colbert.py
+#python -m unittest tests/late_interaction/test_colpali.py
+#python -m unittest tests/late_interaction/test_late_interaction_pipeline.py
 #python -m unittest tests/test_async_astra_multi_vector_table.py
 deactivate
